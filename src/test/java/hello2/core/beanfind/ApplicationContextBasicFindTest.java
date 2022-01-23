b12d580b97e898849d6bce3f3e00e9f99e8978e2
yoonui <yoony0612@ajou.ac.kr>
2022-01-23 16:49:05 +0900

    (22.01.23) 스프링 빈 조회 - 동일한 타입이 둘 이상

---
+++ src/test/java/hello2/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,45 @@
+package hello2.core.beanfind;
+
+import hello2.core.AppConfig;
+import hello2.core.member.MemberService;
+import hello2.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName(){
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType(){
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2(){
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX(){
+        assertThrows(NoSuchBeanDefinitionException.class, () -> {
+            ac.getBean("xxxxx", MemberService.class);
+        }); // 중괄호 안의 로직이 실행되어, 해당하는 예외가 발생해야 테스트가 성공한다.
+    }
+}
