a98498d7dd221b2fa83a559537a30abc50a4e68d
yoonui <yoony0612@ajou.ac.kr>
2022-02-06 16:53:56 +0900

    (22.02.06) 스코프와 프록시

---
+++ src/test/java/hello2/core/web/LogDemoController.java
@@ -0,0 +1,30 @@
+package hello2.core.web;
+
+import hello2.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request){ //사용자의 요청 정보를 받을 수 있음
+        String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass());
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
---
+++ src/test/java/hello2/core/web/LogDemoService.java
@@ -0,0 +1,16 @@
+package hello2.core.web;
+
+import hello2.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
